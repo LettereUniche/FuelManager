ea902f43944999b3315f49488418a13d6da1eb7f
Arkhorse <theillusivemman@gmail.com>
2023-05-13 18:11:05 -0300

    rem using's, rem unneeded check, rem old code, change check

--- VisualStudio/Patches/Panel_Inventory_Examine_RefreshMainWindow.cs
+++ VisualStudio/Patches/Panel_Inventory_Examine_RefreshMainWindow.cs
@@ -1,18 +1,11 @@
 ﻿namespace FuelManager.Patches
 {
-    using Il2Cpp;
-    using HarmonyLib;
-    using UnityEngine;
     [HarmonyPatch(typeof(Panel_Inventory_Examine), nameof(Panel_Inventory_Examine.RefreshMainWindow))]
     internal class Panel_Inventory_Examine_RefreshMainWindow
     {
         private static void Postfix(Panel_Inventory_Examine __instance)
         {
-            //GearItem gearItem = __instance.m_GearItem.GetComponent<GearItem>();
-            //if (__instance == null || __instance.m_GearItem.GetComponent<GearItem>() == null || !FuelUtils.IsFuelItem(__instance.m_GearItem.GetComponent<GearItem>())) return;
-            if (!FuelUtils.IsFuelItem(__instance.m_GearItem)) return; // original code
-
-            if (__instance != null && __instance.m_GearItem != null && FuelUtils.IsFuelItem(__instance.m_GearItem))
+            if (FuelUtils.IsFuelItem(__instance.m_GearItem.GetComponent<GearItem>()))
             {
                 Vector3 position = ButtonUtils.GetBottomPosition(
                                                 __instance.m_Button_Harvest,
@@ -26,8 +19,6 @@
 
                 float litersToDrain = FuelUtils.GetLitersToDrain(__instance.m_GearItem);
                 __instance.m_Button_Unload.GetComponent<Panel_Inventory_Examine_MenuItem>().SetDisabled(litersToDrain < FuelUtils.MIN_LITERS);
-                //if (litersToDrain < FuelUtils.MIN_LITERS) __instance.m_Button_Unload.GetComponent<Panel_Inventory_Examine_MenuItem>().SetDisabled(true);
-                //if (litersToDrain < FuelUtils.MIN_LITERS) __instance.m_Button_Unload.gameObject.SetActive(false);
             }
         }
     }
