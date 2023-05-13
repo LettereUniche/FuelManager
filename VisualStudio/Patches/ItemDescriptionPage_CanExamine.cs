2863d06cf56158ffd5ff7ffea2a2e7e380cec08a
Arkhorse <theillusivemman@gmail.com>
2023-05-13 18:09:47 -0300

    rem using's, change check

--- VisualStudio/Patches/ItemDescriptionPage_CanExamine.cs
+++ VisualStudio/Patches/ItemDescriptionPage_CanExamine.cs
@@ -1,27 +1,15 @@
 ﻿namespace FuelManager.Patches
 {
-    using System;
-    using System.Reflection;
-    using Il2Cpp;
-    using Il2CppTLD.Gear;
-    using MelonLoader;
-    using HarmonyLib;
-    using ModSettings;
-    using ModComponent;
-    using UnityEngine;
     [HarmonyPatch(typeof(ItemDescriptionPage), nameof(ItemDescriptionPage.CanExamine), new Type[] { typeof(GearItem) })]
     internal class ItemDescriptionPage_CanExamine
     {
         private static bool Prefix(GearItem gi, ref bool __result)
         {
-            if (!gi) return false;
-
-            if (FuelUtils.IsFuelItem(gi.GetComponent<GearItem>()))
+            if (FuelUtils.IsFuelItem(gi))
             {
                 __result = true;
                 return false;
             }
-
             return true;
         }
     }
