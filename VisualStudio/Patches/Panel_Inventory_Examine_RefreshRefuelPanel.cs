fd11be0c0f9af8a74bcdc318ac2c3af19ad5f95b
Arkhorse <theillusivemman@gmail.com>
2023-05-13 18:11:27 -0300

    rem using's, rem old code, change check, add additional debug logging

--- VisualStudio/Patches/Panel_Inventory_Examine_RefreshRefuelPanel.cs
+++ VisualStudio/Patches/Panel_Inventory_Examine_RefreshRefuelPanel.cs
@@ -1,48 +1,51 @@
 ﻿namespace FuelManager.Patches
 {
-    using Il2Cpp;
-    using HarmonyLib;
-    using UnityEngine;
-
     [HarmonyPatch(typeof(Panel_Inventory_Examine), nameof(Panel_Inventory_Examine.RefreshRefuelPanel))]
     internal class Panel_Inventory_Examine_RefreshRefuelPanel
     {
         private static bool Prefix(Panel_Inventory_Examine __instance)
         {
-            //GearItem gearItem = __instance.m_GearItem.GetComponent<GearItem>();
-            //bool IsFuelItem = FuelUtils.IsFuelItem(gearItem);
+            if (!FuelUtils.IsFuelItem(__instance.m_GearItem.GetComponent<GearItem>())) return true;
 
-            if (__instance != null && FuelUtils.IsFuelItem(__instance.m_GearItem))
+            __instance.m_RefuelPanel.SetActive(false);
+            __instance.m_Button_Refuel.gameObject.SetActive(true);
+            //__instance.m_Button_RefuelBackground.SetActive(true);
+
+            float currentLiters = FuelUtils.GetIndividualCurrentLiters(__instance.m_GearItem.GetComponent<GearItem>());
+            float capacityLiters = FuelUtils.GetIndividualCapacityLiters(__instance.m_GearItem.GetComponent<GearItem>());
+            float totalCurrent = FuelUtils.GetTotalCurrentLiters(__instance.m_GearItem.GetComponent<GearItem>());
+            float totalCapacity = FuelUtils.GetTotalCapacityLiters(__instance.m_GearItem.GetComponent<GearItem>());
+
+            bool fuelIsAvailable = totalCurrent > FuelUtils.MIN_LITERS;
+            bool flag = fuelIsAvailable && !Mathf.Approximately(currentLiters, capacityLiters);
+
+            __instance.m_Refuel_X.gameObject.SetActive(!flag);
+            __instance.m_Button_Refuel.gameObject.GetComponent<Panel_Inventory_Examine_MenuItem>().SetDisabled(!flag);
+
+            if (!FuelUtils.IsKeroseneLamp(__instance.m_GearItem))
             {
-                __instance.m_RefuelPanel.SetActive(false);
-                __instance.m_Button_Refuel.gameObject.SetActive(FuelUtils.IsFuelItem(__instance.m_GearItem));
-                //__instance.m_Button_Refuel.gameObject.GetComponent<Panel_Inventory_Examine_MenuItem>().SetDisabled(FuelUtils.IsFuelItem(__instance.m_GearItem));
-
-                float currentLiters         = FuelUtils.GetIndividualCurrentLiters(__instance.m_GearItem);
-                float capacityLiters        = FuelUtils.GetIndividualCapacityLiters(__instance.m_GearItem);
-                float totalCurrent          = FuelUtils.GetTotalCurrentLiters(__instance.m_GearItem);
-                float totalCapacity         = FuelUtils.GetTotalCapacityLiters(__instance.m_GearItem);
-
-                bool fuelIsAvailable        = totalCurrent > FuelUtils.MIN_LITERS;
-                bool flag                   = fuelIsAvailable && !Mathf.Approximately(currentLiters, capacityLiters);
-
-                __instance.m_Refuel_X.gameObject.SetActive(!flag);
-                __instance.m_Button_Refuel.gameObject.GetComponent<Panel_Inventory_Examine_MenuItem>().SetDisabled(!flag);
-
-                bool flag2 = Utils.IsGamepadActive();
-
-                __instance.m_RequiresFuelMessage.SetActive(flag && !flag2);
-
-                __instance.m_MouseRefuelButton.SetActive(flag);
-
-                __instance.m_RequiresFuelMessage.SetActive(!flag);
-
-                __instance.m_LanternFuelAmountLabel.text    = $"{FuelUtils.GetLiquidQuantityStringNoOunces(currentLiters)} / {FuelUtils.GetLiquidQuantityStringNoOunces(capacityLiters)}";
-                __instance.m_FuelSupplyAmountLabel.text     = $"{FuelUtils.GetLiquidQuantityStringNoOunces(totalCurrent)} / {FuelUtils.GetLiquidQuantityStringNoOunces(totalCapacity)}";
-
-                __instance.UpdateWeightAndConditionLabels();
+                __instance.m_Button_RefuelBackground.SetActive(true);
+                //__instance.m_Button_RefuelBackground.gameObject.GetComponent<Panel_Inventory_Examine_MenuItem>().SetDisabled(!flag);
             }
-            return true;
+
+            InterfaceManager.GetPanel<Panel_Inventory_Examine>().m_MouseRefuelButton.SetActive(flag);
+            __instance.m_RequiresFuelMessage.SetActive(false);
+
+#if DEBUG
+            FuelManager.Log($"__instance.m_LanternFuelAmountLabel.text: {__instance.m_LanternFuelAmountLabel.text}");
+            FuelManager.Log($"__instance.m_FuelSupplyAmountLabel.text: {__instance.m_FuelSupplyAmountLabel.text}");
+#endif
+
+            __instance.m_LanternFuelAmountLabel.text = $"{FuelUtils.GetLiquidQuantityStringNoOunces(currentLiters)} / {FuelUtils.GetLiquidQuantityStringNoOunces(capacityLiters)}";
+            __instance.m_FuelSupplyAmountLabel.text = $"{FuelUtils.GetLiquidQuantityStringNoOunces(totalCurrent)} / {FuelUtils.GetLiquidQuantityStringNoOunces(totalCapacity)}";
+
+            __instance.UpdateWeightAndConditionLabels();
+#if DEBUG
+            FuelManager.Log($"currentLiters: {currentLiters}, capacityLiters: {capacityLiters}, totalCurrent: {totalCurrent}, totalCapacity: {totalCapacity}, fuelIsAvailable: {fuelIsAvailable}, flag: {flag}");
+            FuelManager.Log($"__instance.m_LanternFuelAmountLabel.text: {__instance.m_LanternFuelAmountLabel.text}");
+            FuelManager.Log($"__instance.m_FuelSupplyAmountLabel.text: {__instance.m_FuelSupplyAmountLabel.text}");
+#endif
+            return false;
         }
     }
 }
