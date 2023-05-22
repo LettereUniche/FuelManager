378c30f49594a170c2db588d2e232543b50334d7
Arkhorse <theillusivemman@gmail.com>
2023-05-22 16:03:24 -0300

    minor code improvements

--- VisualStudio/Patches/Panel_Inventory_Examine_OnRefuel.cs
+++ VisualStudio/Patches/Panel_Inventory_Examine_OnRefuel.cs
@@ -1,21 +1,23 @@
-﻿namespace FuelManager.Patches
+﻿namespace FuelManager
 {
     [HarmonyPatch(typeof(Panel_Inventory_Examine), nameof(Panel_Inventory_Examine.OnRefuel))]
     internal class Panel_Inventory_Examine_OnRefuel
     {
         private static bool Prefix(Panel_Inventory_Examine __instance)
         {
-            if (!FuelUtils.IsFuelItem(__instance.m_GearItem)) return true;
-
-            if (ButtonUtils.IsSelected(__instance.m_Button_Unload))
+            if (__instance != null && FuelUtils.IsFuelItem(__instance.m_GearItem))
             {
-                FuelUtils.Drain(__instance.m_GearItem);
+                if (ButtonUtils.IsSelected(__instance.m_Button_Unload))
+                {
+                    FuelUtils.Drain(__instance.m_GearItem);
+                }
+                else
+                {
+                    FuelUtils.Refuel(__instance.m_GearItem);
+                }
+                return false;
             }
-            else
-            {
-                FuelUtils.Refuel(__instance.m_GearItem);
-            }
-            return false;
+            else return true;
         }
     }
 }
