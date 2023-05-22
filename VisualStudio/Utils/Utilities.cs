2b09d766692a7746598eebb6eef685ae10e3e363
Arkhorse <theillusivemman@gmail.com>
2023-05-22 16:04:47 -0300

    add some more useful methods

--- VisualStudio/Utils/Utilities.cs
+++ VisualStudio/Utils/Utilities.cs
@@ -1,34 +1,76 @@
-﻿namespace FuelManager.Utils
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FuelManager
 {
-    using System;
-    using System.Reflection;
-    using Il2Cpp;
-    using Il2CppTLD.Gear;
-    using MelonLoader;
-    using HarmonyLib;
-    using ModSettings;
-    using ModComponent;
-    using UnityEngine;
-    internal class Utilities
+    internal static class Utilities
     {
-        internal static GearItemData _GearItemData = new();
-        internal static Panel_Inventory_Examine _Panel_Inventory_Examine = new();
-        internal static void SetConditionToMax(GearItem gearItem)
+        public static GearItem GetGearItemPrefab(string name) => GearItem.LoadGearItemPrefab(name).GetComponent<GearItem>();
+        public static ToolsItem GetToolItemPrefab(string name) => GearItem.LoadGearItemPrefab(name).GetComponent<ToolsItem>();
+        public static string? NormalizeName(string name)
         {
-            if (gearItem.CurrentHP < gearItem.GetMaxHPFromRepair())
+            if (name == null)
             {
-                gearItem.SetHaltDecay(true);
-                float num = gearItem.GetMaxHPFromRepair();
-
-                gearItem.CurrentHP += num;
-                gearItem.CurrentHP = Mathf.Clamp(gearItem.CurrentHP, 0f, num);
-
-                gearItem.UpdateDamageShader();
-
-                gearItem.SetHaltDecay(false);
-
-                _Panel_Inventory_Examine.RefreshButton();
+                return null;
             }
+            else
+            {
+                return name.Replace("(Clone)", "").Trim();
+            }
+        }
+
+        [return: NotNullIfNotNull("component")]
+        public static T? GetComponentSafe<T>(this Component? component) where T : Component
+        {
+            return component == null ? default : GetComponentSafe<T>(component.GetGameObject());
+        }
+
+        [return: NotNullIfNotNull("gameObject")]
+        public static T? GetComponentSafe<T>(this GameObject? gameObject) where T : Component
+        {
+            return gameObject == null ? default : gameObject.GetComponent<T>();
+        }
+
+        [return: NotNullIfNotNull("component")]
+        public static T? GetOrCreateComponent<T>(this Component? component) where T : Component
+        {
+            return component == null ? default : GetOrCreateComponent<T>(component.GetGameObject());
+        }
+
+        [return: NotNullIfNotNull("gameObject")]
+        public static T? GetOrCreateComponent<T>(this GameObject? gameObject) where T : Component
+        {
+            if (gameObject == null)
+            {
+                return default;
+            }
+
+            T? result = GetComponentSafe<T>(gameObject);
+
+            if (result == null)
+            {
+                result = gameObject.AddComponent<T>();
+            }
+
+            return result;
+        }
+
+        [return: NotNullIfNotNull("component")]
+        internal static GameObject? GetGameObject(this Component? component)
+        {
+            try
+            {
+                return component == null ? default : component.gameObject;
+            }
+#if DEBUG
+            catch (System.Exception exception)
+            {
+                FuelManager.LogError($"Returning null since this could not obtain a Game Object from the component. Stack trace:\n{exception.Message}");
+            }
+#endif
+#if !DEBUG
+            catch { }
+#endif
+            return null;
         }
     }
 }
