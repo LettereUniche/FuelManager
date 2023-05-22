89e2ed57887db873afe9c91702f5fc2ac8c98d0a
Arkhorse <theillusivemman@gmail.com>
2023-05-22 16:01:52 -0300

    moved BuildInfo to BuildInfo.cs (1edfd9ddb416d672f0301ffb30d5e95e35d51d1f)

--- VisualStudio/FuelManager.cs
+++ VisualStudio/FuelManager.cs
@@ -2,34 +2,11 @@
 {
     internal class FuelManager : MelonMod
     {
-        public static class BuildInfo
-        {
-            #region Mandatory
-            /// <summary>The machine readable name of the mod (no special characters or spaces)</summary>
-            public const string Name            = "FuelManager";
-            /// <summary>Who made the mod</summary>
-            public const string Author          = "The Illusion";
-            /// <summary>Current version (Using Major.Minor.Build) </summary>
-            public const string Version         = "1.0.3";
-            #endregion
+        internal static string SCRAP_METAL_NAME     = "GEAR_ScrapMetal";
+        internal static string SIMPLE_TOOLS_NAME    = "GEAR_HighQualityTools";
+        internal static string QUALITY_TOOLS_NAME   = "GEAR_SimpleTools";
+        internal static string[] FuelContainerWhiteList = { "GEAR_GasCan", "GEAR_GasCanFull", "GEAR_JerrycanRusty", "GEAR_LampFuel", "GEAR_LampFuelFull" };
 
-            #region Optional
-            /// <summary>What the mod does</summary>
-            public const string Description     = "Allows draining laterns and moving fuel between containers";
-            /// <summary>Company that made it</summary>
-            public const string Company         = null;
-            /// <summary>A valid download link</summary>
-            public const string DownloadLink    = "https://github.com/Arkhorse/FuelManager/releases/latest";
-            /// <summary>Copyright info</summary>
-            public const string Copyright       = "2023";
-            /// <summary>Trademark info</summary>
-            public const string Trademark       = null;
-            /// <summary>Product name (should always be the same as the name)</summary>
-            public const string Product         = "FuelManager";
-            /// <summary>Culture info</summary>
-            public const string Culture         = null;
-            #endregion
-        }
         public override void OnInitializeMelon()
         {
             Settings.OnLoad();
@@ -41,15 +18,15 @@
 
         internal static void Log(string message, params object[] parameters)
         {
-            MelonLogger.Msg($"[FuelManager] {message}", parameters);
+            MelonLogger.Msg($"{message}", parameters);
         }
         internal static void LogWarning(string message, params object[] parameters)
         {
-            MelonLogger.Warning($"[FuelManager] {message}", parameters);
+            MelonLogger.Warning($"{message}", parameters);
         }
         internal static void LogError(string message, params object[] parameters)
         {
-            MelonLogger.Error($"[FuelManager] {message}", parameters);
+            MelonLogger.Error($"{message}", parameters);
         }
     }
 }
