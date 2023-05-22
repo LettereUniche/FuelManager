e31b14c21acb3727136e0db3e0b0a60dc2f55300
Arkhorse <theillusivemman@gmail.com>
2023-05-22 16:03:01 -0300

    minor code improvements

--- VisualStudio/Patches/ItemDescriptionPage_CanExamine.cs
+++ VisualStudio/Patches/ItemDescriptionPage_CanExamine.cs
@@ -1,11 +1,11 @@
-﻿namespace FuelManager.Patches
+﻿namespace FuelManager
 {
     [HarmonyPatch(typeof(ItemDescriptionPage), nameof(ItemDescriptionPage.CanExamine), new Type[] { typeof(GearItem) })]
     internal class ItemDescriptionPage_CanExamine
     {
         private static bool Prefix(GearItem gi, ref bool __result)
         {
-            if (FuelUtils.IsFuelItem(gi))
+            if (gi != null && FuelUtils.IsFuelItem(gi))
             {
                 __result = true;
                 return false;
