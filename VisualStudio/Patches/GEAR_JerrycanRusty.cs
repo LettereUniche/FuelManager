833c99c44851c24a98f69fd0db9d34d7647229f1
Arkhorse <theillusivemman@gmail.com>
2023-05-22 16:02:39 -0300

    moved GearItem code to dedicated files to ensure proper functioning of these patches

---
+++ VisualStudio/Patches/GEAR_JerrycanRusty.cs
@@ -0,0 +1,23 @@
+﻿namespace FuelManager
+{
+    [HarmonyPatch(typeof(GearItem), nameof(GearItem.Awake))]
+    internal static class GearItem_Awake_JerrycanRusty
+    {
+        private static void Postfix(GearItem __instance)
+        {
+            if (ItemUtils.NormalizeName(__instance.name) == "GEAR_JerrycanRusty")
+            {
+                __instance.m_Repairable = ItemUtils.GetOrCreateComponent<Repairable>(__instance.gameObject);
+
+                __instance.m_Repairable.m_ConditionIncrease     = 50f;
+                __instance.m_Repairable.m_RequiredGear          = new GearItem[] { ItemUtils.GetGearItemPrefab(FuelManager.SCRAP_METAL_NAME) };
+                __instance.m_Repairable.m_RequiredGearUnits     = new int[] { 1 };
+                __instance.m_Repairable.m_DurationMinutes       = 15;
+                __instance.m_Repairable.m_RepairAudio           = "Play_RepairingMetal";
+                __instance.m_Repairable.m_RepairToolChoices     = new ToolsItem[] { ItemUtils.GetToolItemPrefab("GEAR_HighQualityTools"), ItemUtils.GetToolItemPrefab("GEAR_SimpleTools") };
+                __instance.m_Repairable.m_RequiresToolToRepair  = true;
+                __instance.m_Repairable.m_NeverFail             = true;
+            }
+        }
+    }
+}
---
+++ VisualStudio/Patches/GEAR_LampFuel.cs
@@ -0,0 +1,23 @@
+﻿
+namespace FuelManager
+{
+    [HarmonyPatch(typeof(GearItem), nameof(GearItem.Awake))]
+    internal static class GearItem_Awake_LampFuel
+    {
+        private static void Postfix(GearItem __instance)
+        {
+            if (ItemUtils.NormalizeName(__instance.name) == "GEAR_LampFuel")
+            {
+                __instance.m_Harvest = ItemUtils.GetOrCreateComponent<Harvest>(__instance.gameObject);
+
+                __instance.m_Harvest.m_Audio                = "Play_HarvestingMetalSaw";
+                __instance.m_Harvest.m_DurationMinutes      = 15;
+                __instance.m_Harvest.m_YieldGear            = new GearItem[] { ItemUtils.GetGearItemPrefab(FuelManager.SCRAP_METAL_NAME) };
+                __instance.m_Harvest.m_YieldGearUnits       = new int[] { 1 };
+                __instance.m_Harvest.m_AppliedSkillType     = SkillType.None;
+                __instance.m_Harvest.m_RequiredTools        = new ToolsItem[] { ItemUtils.GetToolItemPrefab("GEAR_Hacksaw") };
+                __instance.m_Harvest.m_GunpowderYield       = 0f;
+            }
+        }
+    }
+}
