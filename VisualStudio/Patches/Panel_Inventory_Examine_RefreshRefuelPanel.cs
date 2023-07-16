701ddbdf42cd3764181f5b08b19cd8d7c4edc087
Arkhorse <theillusivemman@gmail.com>
2023-07-15 21:50:16 -0300

    Update Panel_Inventory_Examine_RefreshRefuelPanel.cs

--- VisualStudio/Patches/Panel_Inventory_Examine_RefreshRefuelPanel.cs
+++ VisualStudio/Patches/Panel_Inventory_Examine_RefreshRefuelPanel.cs
@@ -29,7 +29,7 @@
             catch (NullReferenceException)
             {
 #if DEBUG
-                FuelManager.LogError("Item is not proper, Panel_Inventory_Examine_RefreshRefuelPanel");
+                Logger.LogError("Item is not proper, Panel_Inventory_Examine_RefreshRefuelPanel");
 #endif
             }
 
@@ -41,9 +41,9 @@
 
             __instance.UpdateWeightAndConditionLabels();
 #if DEBUG
-            FuelManager.Log($"currentLiters: {currentLiters}, capacityLiters: {capacityLiters}, totalCurrent: {totalCurrent}, totalCapacity: {totalCapacity}, fuelIsAvailable: {fuelIsAvailable}, flag: {flag}");
-            FuelManager.Log($"__instance.m_LanternFuelAmountLabel.text: {__instance.m_LanternFuelAmountLabel.text}");
-            FuelManager.Log($"__instance.m_FuelSupplyAmountLabel.text: {__instance.m_FuelSupplyAmountLabel.text}");
+            Logger.Log($"currentLiters: {currentLiters}, capacityLiters: {capacityLiters}, totalCurrent: {totalCurrent}, totalCapacity: {totalCapacity}, fuelIsAvailable: {fuelIsAvailable}, flag: {flag}");
+            Logger.Log($"__instance.m_LanternFuelAmountLabel.text: {__instance.m_LanternFuelAmountLabel.text}");
+            Logger.Log($"__instance.m_FuelSupplyAmountLabel.text: {__instance.m_FuelSupplyAmountLabel.text}");
 #endif
             return false; // MUST BE FALSE
         }
