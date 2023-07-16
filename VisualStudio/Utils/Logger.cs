8178c7678a0bcda5230b91e4dd036a20354095fc
Arkhorse <theillusivemman@gmail.com>
2023-07-15 21:47:29 -0300

    Create Logger.cs

---
+++ VisualStudio/Utils/Logger.cs
@@ -0,0 +1,11 @@
+namespace FuelManager
+{
+    public class Logger
+    {
+        public static void Log(string message, params object[] parameters)              => Melon<FuelManager>.Logger.Msg($"{message}", parameters);
+        public static void LogWarning(string message, params object[] parameters)       => Melon<FuelManager>.Logger.Warning($"{message}", parameters);
+        public static void LogError(string message, params object[] parameters)         => Melon<FuelManager>.Logger.Error($"{message}", parameters);
+        public static void LogSeperator(params object[] parameters)                     => Melon<FuelManager>.Logger.Msg("==============================================================================", parameters);
+        public static void LogStarter()                                                 => Melon<FuelManager>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
+    }
+}
