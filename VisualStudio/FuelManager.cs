7103712566a282663a0a68e205bbb2bf3f08530a
Arkhorse <theillusivemman@gmail.com>
2023-07-15 21:46:51 -0300

    rem logging from main class

--- VisualStudio/FuelManager.cs
+++ VisualStudio/FuelManager.cs
@@ -11,22 +11,6 @@
         {
             Settings.OnLoad();
             Spawns.AddToModComponent();
-#if DEBUG
-            Log($"Version: {BuildInfo.Version} Loaded!");
-#endif
-        }
-
-        internal static void Log(string message, params object[] parameters)
-        {
-            MelonLogger.Msg($"{message}", parameters);
-        }
-        internal static void LogWarning(string message, params object[] parameters)
-        {
-            MelonLogger.Warning($"{message}", parameters);
-        }
-        internal static void LogError(string message, params object[] parameters)
-        {
-            MelonLogger.Error($"{message}", parameters);
         }
     }
 }
