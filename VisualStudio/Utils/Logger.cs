5fe02bd9c21241470c5658b6c4ff157501932c7b
Arkhorse <theillusivemman@gmail.com>
2023-07-16 16:46:24 -0300

    add Update testing logger

--- VisualStudio/Utils/Logger.cs
+++ VisualStudio/Utils/Logger.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace FuelManager
 {
     public class Logger
@@ -7,5 +9,14 @@ namespace FuelManager
         public static void LogError(string message, params object[] parameters)         => Melon<FuelManager>.Logger.Error($"{message}", parameters);
         public static void LogSeperator(params object[] parameters)                     => Melon<FuelManager>.Logger.Msg("==============================================================================", parameters);
         public static void LogStarter()                                                 => Melon<FuelManager>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
+        public static void LogUpdate(string[] message)
+        {
+            LogSeperator();
+            for (int i = 0; i < message.Length; i++)
+            {
+                Log(message[i]);
+            }
+            LogSeperator();
+        }
     }
 }
