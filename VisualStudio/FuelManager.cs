fc37f6d3b576fe6210bd1ff4e64203629df77b83
Arkhorse <theillusivemman@gmail.com>
2023-07-15 21:47:00 -0300

    rem constants from main class

--- VisualStudio/FuelManager.cs
+++ VisualStudio/FuelManager.cs
@@ -2,11 +2,6 @@
 {
     internal class FuelManager : MelonMod
     {
-        internal static string SCRAP_METAL_NAME     = "GEAR_ScrapMetal";
-        internal static string SIMPLE_TOOLS_NAME    = "GEAR_HighQualityTools";
-        internal static string QUALITY_TOOLS_NAME   = "GEAR_SimpleTools";
-        internal static string[] FuelContainerWhiteList = { "GEAR_GasCan", "GEAR_GasCanFull", "GEAR_JerrycanRusty", "GEAR_LampFuel", "GEAR_LampFuelFull" };
-
         public override void OnInitializeMelon()
         {
             Settings.OnLoad();
