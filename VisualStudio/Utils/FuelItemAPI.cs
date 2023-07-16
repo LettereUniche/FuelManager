1feabf5dd0310c3e65f9c3d3bde803573fb6cdb9
Arkhorse <theillusivemman@gmail.com>
2023-07-15 21:48:09 -0300

    Update FuelItemAPI.cs

--- VisualStudio/Utils/FuelItemAPI.cs
+++ VisualStudio/Utils/FuelItemAPI.cs
@@ -2,23 +2,25 @@
 {
     public class FuelItemAPI
     {
-
         /// <summary>
         /// This should only be used to add repair to already existing items. Otherwise use the Modcomponent function
         /// </summary>
         /// <param name="gi">The GearItem you want to add repair to</param>
-        public static void AddRepair(GearItem gi)
+        public static void AddRepair(GearItem gi, bool NeverFail = false, float ConditionIncrease = 50f)
         {
-            gi.m_Repairable                          = ItemUtils.GetOrCreateComponent<Repairable>(gi.gameObject);
+            if (gi == null) return;
+            Repairable repairable = ItemUtils.GetOrCreateComponent<Repairable>(gi.gameObject);
 
-            gi.m_Repairable.m_ConditionIncrease      = 50f;
-            gi.m_Repairable.m_RequiredGear           = new GearItem[] { ItemUtils.GetGearItemPrefab(FuelManager.SCRAP_METAL_NAME) };
-            gi.m_Repairable.m_RequiredGearUnits      = new int[] { 1 };
-            gi.m_Repairable.m_DurationMinutes        = 15;
-            gi.m_Repairable.m_RepairAudio            = "Play_RepairingMetal";
-            gi.m_Repairable.m_RepairToolChoices      = new ToolsItem[] { ItemUtils.GetToolItemPrefab("GEAR_HighQualityTools"), ItemUtils.GetToolItemPrefab("GEAR_SimpleTools") };
-            gi.m_Repairable.m_RequiresToolToRepair   = true;
-            gi.m_Repairable.m_NeverFail              = true;
+            repairable.m_RepairAudio = "Play_RepairingMetal";
+            repairable.m_DurationMinutes = 15;
+            repairable.m_ConditionIncrease = ConditionIncrease;
+
+            repairable.m_RequiredGear = ItemUtils.GetItems<GearItem>(Constants.REPAIR_HARVEST_GEAR);
+            repairable.m_RequiredGearUnits = new int[] { 1 };
+
+            repairable.m_RepairToolChoices = ItemUtils.GetItems<ToolsItem>(Constants.REPAIR_TOOLS);
+            repairable.m_RequiresToolToRepair = true;
+            repairable.m_NeverFail = NeverFail;
         }
 
         /// <summary>
@@ -27,14 +29,61 @@
         /// <param name="gi">The GearItem you want to add harvest to</param>
         public static void AddHarvest(GearItem gi)
         {
-            gi.m_Harvest = ItemUtils.GetOrCreateComponent<Harvest>(gi.gameObject);
+            if (gi == null) return;
 
-            gi.m_Harvest.m_Audio            = "Play_HarvestingMetalSaw";
-            gi.m_Harvest.m_DurationMinutes  = 15;
-            gi.m_Harvest.m_YieldGear        = new GearItem[] { ItemUtils.GetGearItemPrefab(FuelManager.SCRAP_METAL_NAME) };
-            gi.m_Harvest.m_YieldGearUnits   = new int[] { 2 };
-            gi.m_Harvest.m_AppliedSkillType = SkillType.None;
-            gi.m_Harvest.m_RequiredTools    = new ToolsItem[] { ItemUtils.GetToolItemPrefab("GEAR_Hacksaw") };
+            Harvest harvest = ItemUtils.GetOrCreateComponent<Harvest>(gi.gameObject);
+
+            harvest.m_Audio = "Play_HarvestingMetalSaw";
+            harvest.m_DurationMinutes = 15;
+
+            harvest.m_YieldGear = ItemUtils.GetItems<GearItem>(Constants.REPAIR_HARVEST_GEAR);
+            harvest.m_YieldGearUnits = new int[] { 2 };
+
+            harvest.m_AppliedSkillType = SkillType.None;
+            harvest.m_RequiredTools = ItemUtils.GetItems<ToolsItem>(Constants.HARVEST_TOOLS);
+            harvest.m_GunpowderYield = 0f;
+        }
+        /// <summary>
+        /// Adds the FuelSourceItem component to the given GearItem
+        /// </summary>
+        /// <param name="gi">GearItem to alter</param>
+        /// <param name="burnHours">m_BurnDurationHours</param>
+        /// <param name="fireAge">m_FireAgeMinutesBeforeAdding</param>
+        /// <param name="fireStartDuration">m_FireStartDurationModifier</param>
+        /// <param name="fireStartSkill">m_FireStartSkillModifier</param>
+        /// <param name="heatIncrease">m_HeatIncrease</param>
+        /// <param name="heatInner">m_HeatInnerRadius</param>
+        /// <param name="heatOuter">m_HeatOuterRadius</param>
+        /// <param name="isBurnt">m_IsBurntInFireTracked</param>
+        /// <param name="isTinder">m_IsTinder</param>
+        /// <param name="isWet">m_IsWet</param>
+        public static void AddFuelSource(GearItem gi,
+                                         float burnHours,
+                                         float fireAge,
+                                         float fireStartDuration,
+                                         float fireStartSkill,
+                                         float heatIncrease,
+                                         float heatInner,
+                                         float heatOuter,
+                                         bool isBurnt,
+                                         bool isTinder,
+                                         bool isWet)
+        {
+            FuelSourceItem fuelSourceItem = ItemUtils.GetOrCreateComponent<FuelSourceItem>(gi.gameObject);
+
+            fuelSourceItem.m_BurnDurationHours = burnHours;
+
+            fuelSourceItem.m_FireAgeMinutesBeforeAdding = fireAge;
+            fuelSourceItem.m_FireStartDurationModifier = fireStartDuration;
+            fuelSourceItem.m_FireStartSkillModifier = fireStartSkill;
+
+            fuelSourceItem.m_HeatIncrease = heatIncrease;
+            fuelSourceItem.m_HeatInnerRadius = heatInner;
+            fuelSourceItem.m_HeatOuterRadius = heatOuter;
+
+            fuelSourceItem.m_IsBurntInFireTracked = isBurnt;
+            fuelSourceItem.m_IsTinder = isTinder;
+            fuelSourceItem.m_IsWet = isWet;
         }
     }
 }
