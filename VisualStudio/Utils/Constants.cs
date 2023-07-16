c59caf63858f6329780ecd3ebaf62b4c5b81c307
Arkhorse <theillusivemman@gmail.com>
2023-07-15 21:47:24 -0300

    Create Constants.cs

---
+++ VisualStudio/Utils/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace FuelManager
+{
+    internal class Constants
+    {
+        internal static string[] FuelContainerWhiteList { get; } = { "GEAR_GasCan", "GEAR_GasCanFull", "GEAR_JerrycanRusty", "GEAR_LampFuel", "GEAR_LampFuelFull" };
+        internal static string[] REPAIR_HARVEST_GEAR { get; } = { "GEAR_ScrapMetal" };
+        internal static string[] HARVEST_TOOLS { get; } = { "GEAR_Hacksaw" };
+        internal static string[] REPAIR_TOOLS { get; } = { "GEAR_SimpleTools", "GEAR_HighQualityTools" };
+    }
+}
