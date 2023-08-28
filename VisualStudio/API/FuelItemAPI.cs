49669ad26c8ff891dfe990af3dda9ebded491f86
Arkhorse <theillusivemman@gmail.com>
2023-08-28 18:36:16 -0300

    Create FuelItemAPI.cs

---
+++ VisualStudio/API/FuelItemAPI.cs
@@ -0,0 +1,182 @@
+﻿namespace FuelManager
+{
+    public static class FuelItemAPI
+    {
+        public static void AddRepair(
+            GearItem? gi,
+            string[] requiredGear,
+            int[] repairUnits,
+            string[] extra,
+            string audio,
+            int duration = 15,
+            float ConditionIncrease = 50,
+            bool requiresTools = true,
+            bool NeverFail = true)
+        {
+            if (gi is null) return;
+
+            GameObject? Target = GetInstancedObject(gi);
+
+            if (Target is null) return;
+
+            if (Target.GetComponent<Repairable>() != null) return;
+
+            Repairable repairable = ItemUtils.GetOrCreateComponent<Repairable>(Target);
+
+            try
+            {
+                repairable.m_RepairAudio            = audio;
+                repairable.m_DurationMinutes        = duration;
+                repairable.m_ConditionIncrease      = ConditionIncrease;
+
+                repairable.m_RequiredGear           = ItemUtils.GetItems<GearItem>(requiredGear);
+                repairable.m_RequiredGearUnits      = repairUnits;
+
+                repairable.m_RepairToolChoices      = ItemUtils.GetItems<ToolsItem>(extra);
+                repairable.m_RequiresToolToRepair   = requiresTools;
+                repairable.m_NeverFail              = NeverFail;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error attempting to add Repairable Component");
+                Logger.Log($"Reason: {e}");
+            }
+        }
+
+        public static void AddHarvest(
+            GearItem gi,
+            string[] YieldGear,
+            int[] YieldUnits,
+            string[] RequiredTools,
+            string audio,
+            SkillType skillType = SkillType.None,
+            int duration = 15
+            )
+        {
+            if (gi is null) return;
+
+            GameObject? Target = GetInstancedObject(gi);
+
+            if (Target is null) return;
+
+            if (Target.GetComponent<Harvest>() != null) return;
+
+            Harvest harvest = ItemUtils.GetOrCreateComponent<Harvest>(Target);
+
+            try
+            {
+                harvest.m_Audio = audio;
+                harvest.m_DurationMinutes = duration;
+
+                harvest.m_YieldGear = ItemUtils.GetItems<GearItem>(YieldGear);
+                harvest.m_YieldGearUnits = YieldUnits;
+
+                harvest.m_AppliedSkillType = skillType;
+                harvest.m_RequiredTools = ItemUtils.GetItems<ToolsItem>(RequiredTools);
+
+                harvest.m_GunpowderYield = 0f;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Error while attempting to add Harvest Component");
+                Logger.Log($"Reason: {e}");
+            }
+
+        }
+
+        public static void AddFuelSource(GearItem gi,
+                                         float burnHours,
+                                         float fireAge,
+                                         float fireStartDuration,
+                                         float fireStartSkill,
+                                         float heatIncrease,
+                                         float heatInner,
+                                         float heatOuter,
+                                         bool isBurnt,
+                                         bool isTinder,
+                                         bool isWet)
+        {
+            if (gi is null) return;
+
+            GameObject? Target = GetInstancedObject(gi);
+
+            if (Target is null) return;
+
+            FuelSourceItem fuelSourceItem                   = ItemUtils.GetOrCreateComponent<FuelSourceItem>(Target);
+
+            try
+            {
+                fuelSourceItem.m_IsBurntInFireTracked           = isBurnt;
+                fuelSourceItem.m_IsWet                          = isWet;
+                fuelSourceItem.m_IsTinder                       = isTinder;
+
+                fuelSourceItem.m_FireAgeMinutesBeforeAdding     = fireAge;
+
+                fuelSourceItem.m_HeatOuterRadius                = heatOuter;
+                fuelSourceItem.m_HeatInnerRadius                = heatInner;
+                fuelSourceItem.m_HeatIncrease                   = heatIncrease;
+
+                fuelSourceItem.m_BurnDurationHours              = burnHours;
+                fuelSourceItem.m_FireStartDurationModifier      = fireStartDuration;
+                fuelSourceItem.m_FireStartSkillModifier         = fireStartSkill;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"Could not add FuelSourceItem to {gi.name}");
+                Logger.Log($"Reason: {e}");
+            }
+        }
+
+        public static GameObject? GetInstancedObject(GearItem gi)
+        {
+            GameObject? Target      = null;
+            GameObject? gameObject  = null;
+            GearItem? prefab        = null;
+
+            if (gi is null) return null;
+
+            try
+            {
+                gameObject = GameObject.Find(gi.name);
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                Logger.LogSeperator();
+                Logger.LogError($"Could not Instantiate item {gi.name}");
+                Logger.Log($"Reason: {e}");
+                Logger.LogSeperator();
+#endif
+            }
+
+            try
+            {
+                if (gameObject != null)
+                {
+                    GearItem component = gameObject.GetComponent<GearItem>();
+                    prefab = ItemUtils.GetGearItemPrefab(ItemUtils.NormalizeName(component.name)!);
+                    Target = prefab.gameObject;
+                }
+                else
+                {
+                    prefab = ItemUtils.GetGearItemPrefab(ItemUtils.NormalizeName(gi.name)!);
+                    Target = prefab.gameObject;
+                }
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                Logger.LogSeperator();
+                Logger.LogError("Attempting to get the Prefab failed");
+                Logger.Log($"GearItem: {gi.name}");
+                Logger.Log($"Reason: {e}");
+                Logger.LogSeperator();
+#endif
+
+                Target = gi.gameObject;
+            }
+
+            return Target;
+        }
+    }
+}
