14a430bd2181d36bfa98103aee24254e16e4b186
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-11-17 19:25:19 -0400

    Update BadMemeException.cs

--- VisualStudio/Utilities/Exceptions/BadMemeException.cs
+++ VisualStudio/Utilities/Exceptions/BadMemeException.cs
@@ -1,5 +1,7 @@
 ﻿// https://github.com/Willster419/RelhaxModpack/blob/master/RelhaxModpack/RelhaxModpack/Common/BadMemeException.cs
 
+using System.Runtime.Serialization;
+
 namespace FuelManager
 {
     /// <summary>
@@ -12,9 +14,8 @@ namespace FuelManager
         /// Throw a bad meme exception.
         /// </summary>
         /// <param name="message">The message to tell the developer why his meme is bad.</param>
-        public BadMemeException(string message) : base(message)
-        {
-
-        }
+        public BadMemeException() : base() { }
+        public BadMemeException(string? message) : base(message) { }
+        public BadMemeException(string? message, Exception? innerException) : base(message, innerException) { }
     }
 }
