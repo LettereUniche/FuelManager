e156ed9232414adf3666205a8d33000e009c5931
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-11-17 19:25:16 -0400

    Update AssetBundleUtils.cs

--- VisualStudio/Utilities/AssetBundleUtils.cs
+++ VisualStudio/Utilities/AssetBundleUtils.cs
@@ -1,55 +1,48 @@
 ﻿using Il2CppInterop.Runtime;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine.AddressableAssets;
 
-namespace FuelManager;
-
-/// <summary>
-/// Alternative asset loading methods to avoid triggering the AssetLoader patches
-/// </summary>
-internal static class AssetBundleUtils
+namespace FuelManager.Utilities
 {
-	/// <summary>
-	/// Loads an asset without triggering the AssetLoader patches
-	/// </summary>
-	public static UnityEngine.Object LoadAsset(AssetBundle assetBundle, string name)
-	{
-		return LoadAsset(assetBundle, name, Il2CppType.Of<UnityEngine.Object>());
-	}
-	/// <summary>
-	/// Loads an asset without triggering the AssetLoader patches
-	/// </summary>
-	public static T? LoadAsset<T>(AssetBundle assetBundle, string name) where T : UnityEngine.Object
-	{
-		return LoadAsset(assetBundle, name, Il2CppType.Of<T>())?.TryCast<T>();
-	}
-	/// <summary>
-	/// Loads an asset without triggering the AssetLoader patches
-	/// </summary>
-	public static UnityEngine.Object LoadAsset(AssetBundle assetBundle, string name, Il2CppSystem.Type type)
-	{
-		if (assetBundle == null)
-		{
-			throw new System.NullReferenceException("The asset bundle cannot be null.");
-		}
-		if (name == null)
-		{
-			throw new System.NullReferenceException("The input asset name cannot be null.");
-		}
-		if (name.Length == 0)
-		{
-			throw new System.ArgumentException("The input asset name cannot be empty.");
-		}
-		if (type == null)
-		{
-			throw new System.NullReferenceException("The input type cannot be null.");
-		}
-		return assetBundle.LoadAsset_Internal(name, type);
-	}
-	/// <summary>
-	/// Load an asset from Addressables
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	/// <param name="assetName"></param>
-	/// <returns></returns>
-	public static T LoadAsset<T>(string assetName) => Addressables.LoadAssetAsync<T>(assetName).WaitForCompletion();
+    /// <summary>
+    /// Alternative asset loading methods to avoid triggering the AssetLoader patches
+    /// </summary>
+    public class AssetBundleUtils
+    {
+        /// <summary>
+        /// Loads an asset without triggering the AssetLoader patches
+        /// </summary>
+        public static UnityEngine.Object LoadAsset(AssetBundle assetBundle, string name)
+        {
+            return LoadAsset(assetBundle, name, Il2CppType.Of<UnityEngine.Object>());
+        }
+
+        /// <summary>
+        /// Loads an asset without triggering the AssetLoader patches
+        /// </summary>
+        public static T? LoadAsset<T>(AssetBundle assetBundle, string name) where T : UnityEngine.Object
+        {
+            return LoadAsset(assetBundle, name, Il2CppType.Of<T>())?.TryCast<T>();
+        }
+
+        /// <summary>
+        /// Loads an asset without triggering the AssetLoader patches
+        /// </summary>
+        public static UnityEngine.Object LoadAsset([DisallowNull] AssetBundle assetBundle, [DisallowNull] string name, [DisallowNull] Il2CppSystem.Type type)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new BadMemeException("The input asset name cannot be empty.");
+            }
+            return assetBundle.LoadAsset_Internal(name, type);
+        }
+
+        /// <summary>
+        /// Load an asset from Addressables
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="assetName"></param>
+        /// <returns></returns>
+        public static T LoadAsset<T>(string assetName) => Addressables.LoadAssetAsync<T>(assetName).WaitForCompletion();
+    }
 }
